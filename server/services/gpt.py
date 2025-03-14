a1d02e4d184ded48187a504f7acdef3c9224e0d5
beomseok <bmtosss@gmail.com>
2025-03-14 11:34:14 +0900

    [SERVER] update image generation logic

--- server/services/gpt.py
+++ server/services/gpt.py
@@ -10,6 +10,6 @@ client = openai.OpenAI(api_key=os.getenv("GPT_API_KEY"))
 def translate_to_english(words: List[str]):
     response = client.responses.create(
         model="gpt-4o-mini",
-        input="please translate each words to english and return only the words:" + " ".join(words)
+        input="please translate each words to english and return only the words with space:" + " ".join(words)
     )
-    return response.output_text
+    return response.output_text.split()
--- server/services/image.py
+++ server/services/image.py
@@ -1,13 +1,22 @@
+from typing import List
 from dto.ImageDto import ImageDto
 from utils.uuid import make_uuid_with_prefix_and_suffix
+from .gpt import translate_to_english
 
 def generate_diary_image(imageDto: ImageDto):
     image_path = generate_image_path(imageDto.username, imageDto.month, imageDto.date)
     
+    # 영어로 번역
+    translated_words = translate_to_english(imageDto.tokens)
+    
     # 이미지 생성
+    generate_image_by_SD(translated_words, imageDto.style, image_path)
     
     return image_path
     
 def generate_image_path(username: str, month: str, date: str):
     image_path = make_uuid_with_prefix_and_suffix(f"./images/{username}-{month}-{date}", ".png")
     return image_path
+
+def generate_image_by_SD(translated_words: List[str], style: str, image_path: str):
+    pass
