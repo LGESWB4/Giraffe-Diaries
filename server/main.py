71252e569608a890a912ca46f445335479efb040
beomseok <bmtosss@gmail.com>
2025-03-14 11:31:38 +0900

    [SERVER] add settings related to cors

--- server/main.py
+++ server/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from services.gpt import translate_to_english
 from api import image_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(image_router)
