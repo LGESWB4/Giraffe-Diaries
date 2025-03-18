b0b6756a34afe7638dd00b6c534827a1865245a0
Kimdeokryun <96904134+Kimdeokryun@users.noreply.github.com>
2025-03-18 15:33:03 +0900

    [APP] Merge view,write-diary complete

--- Giraffe_Diaries/lib/services/api_service.dart
+++ Giraffe_Diaries/lib/services/api_service.dart
@@ -41,4 +41,39 @@ class ApiService {
   static String getImageUrl(String imagePath) {
     return '$baseUrl/image/$imagePath';
   }
+
+
+  static Future<String> chatAPI({
+    required String username,
+    required String inputWord,
+    required String month,
+    required String date,
+    required String styleWord,
+    required String emotionQuery,
+  }) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/api/chat/'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'username': username,
+          'input_word': inputWord,
+          'month': month,
+          'date': date,
+          'style_word': styleWord,
+          'emotion_query': emotionQuery,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        print("data: ${data}");
+        return data['image_path'];
+      } else {
+        throw Exception('Failed to generate image');
+      }
+    } catch (e) {
+      throw Exception('Error generating image: $e');
+    }
+  }
 }
